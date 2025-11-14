0a8f2683105ba7fa225b581a79e2c65d22250e7f
Mike Gurney <mike.gurney@cfacorp.com>
2025-11-14 10:48:37 -0500

    shuffling things around

--- pkg/kafka/subscriber.go
+++ pkg/kafka/subscriber.go
@@ -567,6 +567,14 @@ func (h messageHandler) processMessage(
 
 	h.logger.Trace("Received message from Kafka", receivedMsgLogFields)
 
+	ctx = setPartitionToCtx(ctx, kafkaMsg.Partition)
+	ctx = setPartitionOffsetToCtx(ctx, kafkaMsg.Offset)
+	ctx = setMessageTimestampToCtx(ctx, kafkaMsg.Timestamp)
+	ctx = setMessageKeyToCtx(ctx, kafkaMsg.Key)
+
+	ctx, cancelCtx := context.WithCancel(ctx)
+	defer cancelCtx()
+
 	var (
 		msg *message.Message
 		err error
@@ -580,15 +588,7 @@ func (h messageHandler) processMessage(
 		// resend will make no sense, stopping consumerGroupHandler
 		return errors.Wrap(err, "message unmarshal failed")
 	}
-
-	ctx = setPartitionToCtx(msg.Context(), kafkaMsg.Partition)
-	ctx = setPartitionOffsetToCtx(ctx, kafkaMsg.Offset)
-	ctx = setMessageTimestampToCtx(ctx, kafkaMsg.Timestamp)
-	ctx = setMessageKeyToCtx(ctx, kafkaMsg.Key)
-
-	ctx, cancelCtx := context.WithCancel(ctx)
 	msg.SetContext(ctx)
-	defer cancelCtx()
 
 	receivedMsgLogFields = receivedMsgLogFields.Add(watermill.LogFields{
 		"message_uuid": msg.UUID,
